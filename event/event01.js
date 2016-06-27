a66a2e177b0db8b966b8ac43da5a1ad761c431e3
wangxingchao <772704457@qq.com>
2016-06-27 23:06:14 +0800

    在菜鸟网站山用到的demo

---
+++ event/event01.js
@@ -0,0 +1,24 @@
+// 引入 events 模块
+var events = require('events');
+
+// 创建eventEmitter 对象
+var eventEmitter = new events.EventEmitter();
+
+// 绑定事件并处理
+//eventEmitter.on('eventName',eventHandler);
+// 触发事件
+//eventEmitter.emit('eventName');
+
+// 创建时间处理程序
+var connectHandler = function connected(){
+  console.log('连接成功');
+  // 出发data_received 事件
+  eventEmitter.emit('data_received');
+}
+//绑定connection事件
+eventEmitter.on('connection',connectHandler);
+eventEmitter.on('data_received',function(){
+  console.log('数据接受成功');
+});
+eventEmitter.emit('connection');
+console.log('程序执行完毕');
