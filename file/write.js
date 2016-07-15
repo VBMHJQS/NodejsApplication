71f7ef77deeb7f5bc9598ef8fe2dc5ce2f5ba3c7
wangxc <wangxc@5iquant.com>
2016-07-15 17:51:11 +0800

    测试写入

--- file/write.js
+++ file/write.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 var iconv = require('iconv-lite');
-var path = 'F:/logs/20160714.log';
+var path = 'F:/logs/20160715.log';
 var data = '2016/06/17 22:58:32.965自动交易信息:帐户[kctest]商品[IF1610]类型[买开]数量[1]价格[2088.00000000]注释[TB_G_SWK_IF000_30MIN_V2_1]\r\r\n2016/06/17 22:58:32.966自动交易信息:帐户[kctest]商品[IF1610]类型[买开]数量[1]价格[2088.00000000]注释[TB_G_SWK_IF000_30MIN_V2_1]\r\r\n2016/06/17 22:58:32.965自动交易信息:帐户[kctest]商品[rb1610]类型[买开]数量[1]价格[2088.00000000]注释[wahaha]\r\r\n';
 writeFile(path);
 function writeFile(file){
